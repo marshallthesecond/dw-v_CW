6505fb9492b758a0191880f32d407420a80ddabd
marshallthesecond <shmardonov05@gmail.com>
2026-03-12 18:51:27 +0500

    1. Sidebar content was moved to app.py file for the global access; 2. Tabs were improved using on_change state tracking; 3. Containers with borders were applied throughtout the upload_and_overview tab and components were arrenged for better UX.

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
 import streamlit as st
 import pandas as pd
+import time
 
 from tabs import upload_and_overview
 from tabs import cleaning_and_prep
+from tabs import visualization
+from tabs import export_and_report
 
-# Set page configuration
+# Page configuration
 st.set_page_config(
     page_title="Data Wrangler and Visualizer", 
     layout="wide",
     page_icon="👋")
 
-# Pages
-# homepage = st.Page("Home.py", title="Home", icon="🏠")
-# page_1 = st.Page("pages/upload_and_overview.py", title="Upload & Overview", icon="📈")
-# page_2 = st.Page("pages/cleaning_and_prep.py", title="Cleaning & Prep", icon="🧹")
-# page_3 = st.Page("pages/visualization.py", title="Visualization", icon="📊")
-# page_4 = st.Page("pages/export_and_report.py", title="Modeling", icon="🤖")
 
-# pg = st.navigation([page_1, page_2, page_3, page_4])
-# pg.run()
+# Sidebar with progress bar
+st.sidebar.header("Sidebar settings")
+progress_bar = st.progress(0)
+status_text = st.sidebar.empty()
+for i in range(100):
+    status_text.info("%i%% Complete" % i)
+    progress_bar.progress(i + 1)
+    time.sleep(0.005)
+progress_bar.empty()
+status_text.empty()
+
 
 # Tabs
-tab1, tab2, tab3, tab4 = st.tabs(["Upload and Overview", "Cleaning and Prep", "Visuallization", "Modeling"])
+tab1, tab2, tab3, tab4 = st.tabs(["Upload and Overview", "Cleaning and Prep", "Visuallization", "Export and Report"], on_change="rerun")
 
-with tab1:
-    upload_and_overview.upload_overview()
-with tab2:
-    cleaning_and_prep.clean_prep()
+if tab1.open:
+    with st.spinner("Loading Upload and Overview..."):
+        time.sleep(1)
+    with tab1:
+        upload_and_overview.upload_overview()
+if tab2.open:
+    with st.spinner("Loading Cleaning and Prep..."):
+        time.sleep(1)
+    with tab2:
+        cleaning_and_prep.clean_prep()
+if tab3.open:
+    with st.spinner("Loading Visualization..."):
+        time.sleep(1)
+    with tab3:
+        st.write("Visualization")
+if tab4.open:
+    with st.spinner("Loading Export and Report..."):
+        time.sleep(1)
+    with tab4:
+        export_and_report.export_report()
--- tabs/upload_and_overview.py
+++ tabs/upload_and_overview.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import time
 import pandas as pd
-import numpy as np
 
 def upload_overview():
 
@@ -16,21 +14,10 @@ def upload_overview():
     st.title("Your Data Wrangler and Visualizer")
     st.write("Welcome to your Data Wrangler and Visualizer! Upload your data and explore them with ease!")
 
-    # Sidebar with progress bar
-    st.sidebar.header("Sidebar settings")
-    progress_bar = st.progress(0)
-    status_text = st.sidebar.empty()
-    for i in range(100):
-        status_text.info("%i%% Complete" % i)
-        progress_bar.progress(i + 1)
-        time.sleep(0.005)
-    progress_bar.empty()
-    status_text.empty()
-
-    st.info("Choose only CSV, EXCEL or JSON file!")
-
     # File uploader
-    uploaded_file = st.file_uploader("Upload your file", type=["csv", "excel", "json"])
+    with st.container(border=True):
+        # st.write("Upload your dataset here:")
+        uploaded_file = st.file_uploader("Upload your file", type=["csv", "excel", "json"])
 
     if uploaded_file is not None:
         if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
@@ -44,38 +31,49 @@ def upload_overview():
         st.session_state["data"] = df
         
         # Display dataset overview
-        st.markdown("**Data Preview:**")
-        st.write(df)
+        st.header("Data Preview:")
+        if st.checkbox("Show full dataset"):
+            st.write(df)
+
         # Data shape
-        if st.sidebar.checkbox("Show Data Shape"):
+        with st.container(border=True):
             st.markdown("**Data Shape:**")
-            st.write("Number of rows: ", df.shape[0])
-            st.write("Number of columns: ", df.shape[1])
-        # Column names
-        if st.sidebar.checkbox("Show Column Names"):
-            st.markdown("**Column Names:**")
-            st.write(df.columns.tolist())
-        # Data types
-        if st.sidebar.checkbox("Show Data Types"):
-            st.markdown("**Data Types:**")
-            st.write(df.dtypes)
-        # Summary stats
-        if st.sidebar.checkbox("Show Summary Statistics"):
-            st.markdown("**Summary Statistics:**")
-            st.write(df.describe(include='all'))
-        # Missing values
-        if st.sidebar.checkbox("Show Missing Values"):
-            missing = pd.DataFrame({
-                "Missing Count": df.isnull().sum(),
-                "Missing Percentage (%)": (df.isnull().sum() / len(df) * 100).round(2)
-            })
-            st.markdown("**Missing Values:**")
-            st.write(missing)
+            col1, col2 = st.columns(2, border=True)
+            col1.metric("Rows", df.shape[0])
+            col2.metric("Columns", df.shape[1])
+
         # Duplicate rows
-        if st.sidebar.checkbox("Duplicate Rows"):
-            st.markdown("**Duplicate Rows:**")
+        with st.expander("Duplicate Rows"):
+            # st.markdown("**Duplicate Rows:**")
             if df.duplicated().sum() > 0:
                 st.markdown("**Number of Duplicate Rows:**")
                 st.write(df[df.duplicated()])
             else:   
                 st.info("No duplicate rows found")
+
+        with st.container(horizontal=True):
+            # Column names
+            # if st.sidebar.checkbox("Show Column Names"):
+            with st.container(border=True):
+                st.markdown("**Column Names:**")
+                st.write(df.columns)
+            # Data types
+            # if st.sidebar.checkbox("Show Data Types"):
+            with st.container(border=True):
+                st.markdown("**Data Types:**")
+                st.write(df.dtypes)
+            # Missing values
+            with st.container(border=True):
+                missing = pd.DataFrame({
+                    "Missing Count": df.isnull().sum(),
+                    "Missing Percentage (%)": (df.isnull().sum() / len(df) * 100).round(2)
+                })
+                st.markdown("**Missing Values:**")
+                st.write(missing)
+        # Summary stats
+        with st.container(border=True):
+            st.markdown("**Summary Statistics:**")
+            st.write(df.describe(include='all'))
+        
+
+        
